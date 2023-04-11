e0203b8d5b0551b62d29d19f050c0e6c141b29ae
kannanATexcelforte <kannan@excelforte.com>
2023-04-11 07:20:09 +0530

    11.04.2023
    239. data types has completed

---
+++ datatypes.js
@@ -0,0 +1,8 @@
+var a = 10;
+var b = 'Hello';
+var c = true;
+var d = { firstName: 'Adam', lastName: 'Smith' };
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
---
+++ datatypes.ts
@@ -0,0 +1,9 @@
+var a: number = 10;
+var b: string = 'Hello';
+var c: boolean = true;
+var d: any = { firstName: 'Adam', lastName: 'Smith' };
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
